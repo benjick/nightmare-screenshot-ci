e238354e51a30d55ebdc9f5b8f717cd73629e13b
Max Malm <benjick@dumfan.net>
2016-03-18 01:32:57 +0100

    test

--- after_success.js
+++ after_success.js
@@ -3,7 +3,10 @@
 const env = process.env;
 const fetch = require('node-fetch');
 
+console.log('Triggered if Travis wins');
+
 if (env.TRAVIS_PULL_REQUEST) {
+  console.log('This is a pull request');
   const url = `https://api.github.com/repos/${env.TRAVIS_REPO_SLUG}/pulls/${env.TRAVIS_PULL_REQUEST}/comments`;
   fetch(url, {
     method: 'POST',
@@ -12,12 +15,14 @@ if (env.TRAVIS_PULL_REQUEST) {
     },
     body: {
       'body': 'Nice change',
-      'in_reply_to': 4
+      'in_reply_to': 1
     },
   })
 	.then(function(res) {
 		return res.json();
 	}).then(function(json) {
 		console.log(json);
-	});
+	}).catch(function(err) {
+    console.log(err);
+  });
 }
