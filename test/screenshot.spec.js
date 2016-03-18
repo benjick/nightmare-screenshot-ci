9eb86630ebf00d8d2ec37cb005b9d6a4c82f98f7
Max Malm <benjick@dumfan.net>
2016-03-18 01:08:15 +0100

    test

--- test/screenshot.spec.js
+++ test/screenshot.spec.js
@@ -1,14 +1,15 @@
 'use strict';
 
 const server = require('../server.js');
+const fetch = require('node-fetch');
+const port = 3033;
+const url = `http://localhost:${port}`;
 
 require('mocha-generators').install();
 process.setMaxListeners(0);
 
-describe('Take screenshot', function() {
-  this.timeout(150000);
-  const port = 3033;
-  const url = `http://localhost:${port}`;
+describe('Tests', function() {
+  this.timeout(15000);
 
   let nightmare;
   let newServer;
@@ -23,7 +24,16 @@ describe('Take screenshot', function() {
     server.stop(newServer);
   });
 
-  it("of startpage", function * () {
+
+  it("Can be accessed with fetch", function * () {
+    const page = yield fetch(url)
+    .then(function(res) {
+  		return res.text();
+  	})
+    expect(page).to.not.equal('');
+  });
+
+  it("can take screenshot of startpage", function * () {
     const title = yield nightmare
       .goto(url)
       .Screenshot.take('widescreen', 1920, 1080)
